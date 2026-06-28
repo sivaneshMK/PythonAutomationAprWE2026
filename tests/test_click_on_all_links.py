af4c1fe3ad799ebb6d11cdb31aabbac25868c5db
sivaneshMK <sivaneshmk@gmail.com>
2026-06-28 10:41:19 +0530

    new

--- tests/test_click_on_all_links.py
+++ tests/test_click_on_all_links.py
@@ -16,3 +16,10 @@ def test_get_all_courses(get_driver3):
     for course in courses:
         print(course.text)
 
+@pytest.mark.regression
+def test_click_on_all(get_driver3):
+    driver = get_driver3
+    courses = driver.find_elements(By.XPATH, "//span[text()='Know more']/parent::span/parent::div/parent::a/parent::div")
+    for course in courses:
+        course.click()
+
